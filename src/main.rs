414e19beb07cddf3636180450ef97d2fa296996f
NMaster23 <nmmaster22@gmail.com>
2026-04-18 17:38:48 -0700

    Worked on Locked Camera and Terrain
    Worked on Locked Camera and Terrain

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 use bevy::prelude::*;
 use avian3d::prelude::*;
+use bevy_voxel_world::prelude::*;
 
 #[derive(Component)]
-struct PlayerData {
+struct Player;
+
+#[derive(Component)]
+pub struct PlayerData {
     health: i32,
     player_name: String,
     player_id: u32,
 }
+
+#[derive(Resource, Clone, Default)]
+struct MyWorld;
+/*
+impl VoxelWorldConfig for MyWorld {
+    type MaterialIndex = u8;
+    type ChunkUserBundle = ();
+
+    // All options have defaults, so you only need to add the ones you want to modify.
+    // For a full list, see src/configuration.rs
+    fn spawning_distance(&self) -> u32 {
+        25
+    }
+}
+    */
 
 fn spawn_player(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
     let player = PlayerData {
@@ -32,31 +51,31 @@ fn player_movement(keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<(
     for (mut linear_velocity, mut player) in query.iter_mut() {
         let mut move_direction = Vec3::ZERO;
         if keyboard_input.pressed(KeyCode::KeyW) && player.health > 0 {
-            move_direction.z -= 0.1;
+            move_direction.z -= 1.0;
         }
         if keyboard_input.pressed(KeyCode::KeyS) && player.health > 0 {
-            move_direction.z += 0.1;
+            move_direction.z += 1.0;
         }
         if keyboard_input.pressed(KeyCode::KeyA) && player.health > 0 {
-            move_direction.x -= 0.1;
+            move_direction.x -= 1.0;
         }
         if keyboard_input.pressed(KeyCode::KeyD) && player.health > 0 {
-            move_direction.x += 0.1;
+            move_direction.x += 1.0;
         }
         if keyboard_input.pressed(KeyCode::Space) && player.health > 0 {
-            move_direction.y += 0.1;
+            move_direction.y += 1.0;
         }
         if keyboard_input.pressed(KeyCode::ArrowUp) && player.health > 0 {
-            move_direction.z -= 0.1;
+            move_direction.z -= 1.0;
         }
         if keyboard_input.pressed(KeyCode::ArrowDown) && player.health > 0 {
-            move_direction.z += 0.1;
+            move_direction.z += 1.0;
         }
         if keyboard_input.pressed(KeyCode::ArrowLeft) && player.health > 0 {
-            move_direction.x -= 0.1;
+            move_direction.x -= 1.0;
         }
         if keyboard_input.pressed(KeyCode::ArrowRight) && player.health > 0 {
-            move_direction.x += 0.1;
+            move_direction.x += 1.0;
         }
         
         linear_velocity.0 = move_direction;
@@ -73,6 +92,7 @@ fn setup(
         RigidBody::Static,
         Collider::cylinder(4.0, 0.1),
         Mesh3d(meshes.add(Circle::new(4.0))),
+        Transform::from_xyz(0.0, -0.5, 0.0),
         MeshMaterial3d(materials.add(Color::WHITE)),
     ));
     // light
@@ -88,12 +108,15 @@ fn setup(
         Camera3d::default(),
         Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
+    
 }
 
 fn main() {
-        App::new()
+    App::new()
         .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
+        //.add_plugins(VoxelWorldPlugin::with_config(MyWorld))
+        .insert_resource(Gravity(Vec3::new(0.0, -30.0, 0.0)))
         .add_systems(Startup, (spawn_player, setup))
-        .add_systems(Update, player_movement)
+        .add_systems(Update, (player_movement))
         .run();
 }
