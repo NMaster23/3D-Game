f006f30d8c98fcef24421e4677f5c04111c460d3
NMaster23 <nmmaster22@gmail.com>
2026-04-19 17:20:40 -0700

    Added Player 3D Model and Updated Jumping
    Added Player 3D Model and Updated Jumping

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use bevy::prelude::*;
 use avian3d::prelude::*;
-use bevy_voxel_world::prelude::*;
 
 #[derive(Component)]
 struct Player;
@@ -12,22 +11,7 @@ pub struct PlayerData {
     player_id: u32,
 }
 
-#[derive(Resource, Clone, Default)]
-struct MyWorld;
-/*
-impl VoxelWorldConfig for MyWorld {
-    type MaterialIndex = u8;
-    type ChunkUserBundle = ();
-
-    // All options have defaults, so you only need to add the ones you want to modify.
-    // For a full list, see src/configuration.rs
-    fn spawning_distance(&self) -> u32 {
-        25
-    }
-}
-    */
-
-fn spawn_player(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
+fn spawn_player(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, asset_server: Res<AssetServer>) {
     let player = PlayerData {
         health: 100,
         player_name: String::from("Player1"),
@@ -36,8 +20,7 @@ fn spawn_player(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut ma
     commands.spawn((
         RigidBody::Dynamic,
         Collider::cuboid(1.0, 1.0, 1.0),
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        MeshMaterial3d(materials.add(Color::srgb_u8(200, 50, 50))),
+        SceneRoot(asset_server.load(GltfAssetLabel::Scene(0).from_asset("Player\\player.glb"))),
         AngularVelocity(Vec3::new(2.5, 3.5, 1.5)),
         PlayerData {
             health: 100,
@@ -48,6 +31,7 @@ fn spawn_player(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut ma
 }
 
 fn player_movement(keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<(&mut LinearVelocity, &mut PlayerData)>) {
+    let mut jump_times = 0;
     for (mut linear_velocity, mut player) in query.iter_mut() {
         let mut move_direction = Vec3::ZERO;
         if keyboard_input.pressed(KeyCode::KeyW) && player.health > 0 {
@@ -62,8 +46,15 @@ fn player_movement(keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<(
         if keyboard_input.pressed(KeyCode::KeyD) && player.health > 0 {
             move_direction.x += 1.0;
         }
-        if keyboard_input.pressed(KeyCode::Space) && player.health > 0 {
-            move_direction.y += 1.0;
+        if keyboard_input.just_pressed(KeyCode::Space) && player.health > 0 {
+            linear_velocity.y = 10.0;
+            jump_times += 1;
+        }
+        if jump_times >= 2 {
+            let mut timer = Timer::from_seconds(1.0, TimerMode::Once);
+            if timer.just_finished() ==  true {
+                jump_times = 0;
+            }
         }
         if keyboard_input.pressed(KeyCode::ArrowUp) && player.health > 0 {
             move_direction.z -= 1.0;
@@ -78,7 +69,7 @@ fn player_movement(keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<(
             move_direction.x += 1.0;
         }
         
-        linear_velocity.0 = move_direction;
+        linear_velocity.x = move_direction.x * 5.0; linear_velocity.z = move_direction.z * 5.0;
     }
 }
 
@@ -90,9 +81,8 @@ fn setup(
     // circular base
     commands.spawn((
         RigidBody::Static,
-        Collider::cylinder(4.0, 0.1),
-        Mesh3d(meshes.add(Circle::new(4.0))),
-        Transform::from_xyz(0.0, -0.5, 0.0),
+        Mesh3d(meshes.add(Cylinder::new(10.0, 0.5))),
+        Collider::cylinder(10.0, 0.5),
         MeshMaterial3d(materials.add(Color::WHITE)),
     ));
     // light
@@ -111,12 +101,24 @@ fn setup(
     
 }
 
+fn lock_camera(query: Query<(&Transform, &PlayerData), With<Player>>, mut camera_query: Query<&mut Transform, (With<Camera3d>, Without<Player>)>) {
+    if let Ok((player_transform, _)) = query.single() {
+        for mut camera_transform in camera_query.iter_mut() {
+            *camera_transform = Transform::from_xyz(
+                player_transform.translation.x - 2.5,
+                player_transform.translation.y + 4.5,
+                player_transform.translation.z + 9.0,
+            ).looking_at(player_transform.translation, Vec3::Y);
+        }
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
         //.add_plugins(VoxelWorldPlugin::with_config(MyWorld))
-        .insert_resource(Gravity(Vec3::new(0.0, -30.0, 0.0)))
+        .insert_resource(Gravity(Vec3::new(0.0, -35.0, 0.0)))
         .add_systems(Startup, (spawn_player, setup))
-        .add_systems(Update, (player_movement))
+        .add_systems(Update, (player_movement, lock_camera))
         .run();
 }
