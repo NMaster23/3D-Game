8696fbf6272c270fee15a6452ddd68564855c85d
NMaster23 <nmmaster22@gmail.com>
2026-05-10 19:08:12 -0700

    Implemented a portal based on docs and decreased jump timer

--- src/main.rs
+++ src/main.rs
@@ -154,7 +154,7 @@ fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>, mut grap
             player_name: "Admin".into(),
             player_id: 1,
             jumps: 2,
-            jump_timer: Timer::from_seconds(2.0, TimerMode::Once)
+            jump_timer: Timer::from_seconds(1.0, TimerMode::Once)
         },
         CharacterController {
             move_direction: Vec3::ZERO,
@@ -461,6 +461,63 @@ fn setup(
             ));
         });
     });
+}
+
+fn particle_effects(mut commands: Commands, mut effects: ResMut<Assets<EffectAsset>>) {
+    let mut color_gradient1 = bevy_hanabi::Gradient::new();
+    color_gradient1.add_key(0.0, Vec4::new(4.0, 4.0, 4.0, 1.0));
+    color_gradient1.add_key(0.1, Vec4::new(4.0, 4.0, 0.0, 1.0));
+    color_gradient1.add_key(0.9, Vec4::new(4.0, 0.0, 0.0, 1.0));
+    color_gradient1.add_key(1.0, Vec4::new(4.0, 0.0, 0.0, 0.0));
+
+    let mut size_gradient1 = bevy_hanabi::Gradient::new();
+    size_gradient1.add_key(0.3, Vec3::new(0.2, 0.02, 1.0));
+    size_gradient1.add_key(1.0, Vec3::splat(0.0));
+
+    let writer = ExprWriter::new();
+
+    let init_pos = SetPositionCircleModifier {
+        center: writer.lit(Vec3::ZERO).expr(),
+        axis: writer.lit(Vec3::Z).expr(),
+        radius: writer.lit(4.).expr(),
+        dimension: ShapeDimension::Surface,
+    };
+
+    let age = writer.lit(0.).expr();
+    let init_age = SetAttributeModifier::new(Attribute::AGE, age);
+
+    // Give a bit of variation by randomizing the lifetime per particle
+    let lifetime = writer.lit(0.6).uniform(writer.lit(1.3)).expr();
+    let init_lifetime = SetAttributeModifier::new(Attribute::LIFETIME, lifetime);
+
+    // Add drag to make particles slow down a bit after the initial acceleration
+    let drag = writer.lit(2.).expr();
+    let update_drag = LinearDragModifier::new(drag);
+
+    let mut module = writer.finish();
+
+    let tangent_accel = TangentAccelModifier::constant(&mut module, Vec3::ZERO, Vec3::Z, 30.);
+
+    let effect1 = effects.add(
+        EffectAsset::new(16384, SpawnerSettings::rate(5000.0.into()), module)
+            .with_name("portal")
+            .init(init_pos)
+            .init(init_age)
+            .init(init_lifetime)
+            .update(update_drag)
+            .update(tangent_accel)
+            .render(ColorOverLifetimeModifier::new(color_gradient1))
+            .render(SizeOverLifetimeModifier {
+                gradient: size_gradient1,
+                screen_space_size: false,
+            })
+            .render(OrientModifier::new(OrientMode::AlongVelocity)),
+    );
+    commands.spawn((
+        Name::new("portal"),
+        ParticleEffect::new(effect1),
+        Transform::from_xyz(0.0, 10.0, 0.0),
+    ));
 }
 
 pub fn setup_lighting(mut query: Query<&mut Visibility, With<Lighting>>, keycode: Res<ButtonInput<KeyCode>>) {
