31064a652870df4e1080498d433a01d7222c3898
NMaster23 <nmmaster22@gmail.com>
2026-05-11 17:21:23 -0700

    Used AI to convert coloring into Vec4 for particles

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use bevy::{post_process::bloom::Bloom, prelude::*, ui::RelativeCursorPosition, window::{CursorGrabMode, CursorOptions, WindowResolution}, input::mouse::AccumulatedMouseMotion, color::palettes::css, render::view::Hdr, core_pipeline::tonemapping::Tonemapping};
+use bevy::{color::palettes::css, core_pipeline::tonemapping::Tonemapping, input::mouse::AccumulatedMouseMotion, math::VectorSpace, post_process::bloom::Bloom, prelude::*, render::view::Hdr, ui::RelativeCursorPosition, window::{CursorGrabMode, CursorOptions, WindowResolution}};
 use avian3d::prelude::*;
 use std::{time::Duration, ops::{Deref, DerefMut}};
 use rand::prelude::*;
 use bevy_embedded_assets::EmbeddedAssetPlugin;
 use bevy_hanabi::*;
-use bevy_hanabi::prelude::Gradient as HanabiGradient;
 
 #[derive(Component)]
 pub struct Lighting;
@@ -464,58 +463,24 @@ fn setup(
 }
 
 fn particle_effects(mut commands: Commands, mut effects: ResMut<Assets<EffectAsset>>) {
-    let mut color_gradient1 = bevy_hanabi::Gradient::new();
-    color_gradient1.add_key(0.0, Vec4::new(4.0, 4.0, 4.0, 1.0));
-    color_gradient1.add_key(0.1, Vec4::new(4.0, 4.0, 0.0, 1.0));
-    color_gradient1.add_key(0.9, Vec4::new(4.0, 0.0, 0.0, 1.0));
-    color_gradient1.add_key(1.0, Vec4::new(4.0, 0.0, 0.0, 0.0));
+    let mut gradient = bevy_hanabi::Gradient::new();
+    gradient.add_key(0.0, Vec4::new(0.9, 0.98, 1.0, 1.0));
+    gradient.add_key(0.15, Vec4::new(0.0, 0.843, 1.0, 1.0));
+    gradient.add_key(0.6, Vec4::new(0.0, 0.2, 0.7, 0.5));
+    gradient.add_key(1.0, Vec4::new(0.0, 0.0, 0.0, 0.0));
 
-    let mut size_gradient1 = bevy_hanabi::Gradient::new();
-    size_gradient1.add_key(0.3, Vec3::new(0.2, 0.02, 1.0));
-    size_gradient1.add_key(1.0, Vec3::splat(0.0));
-
-    let writer = ExprWriter::new();
-
-    let init_pos = SetPositionCircleModifier {
-        center: writer.lit(Vec3::ZERO).expr(),
-        axis: writer.lit(Vec3::Z).expr(),
-        radius: writer.lit(4.).expr(),
-        dimension: ShapeDimension::Surface,
-    };
-
-    let age = writer.lit(0.).expr();
-    let init_age = SetAttributeModifier::new(Attribute::AGE, age);
-
-    // Give a bit of variation by randomizing the lifetime per particle
-    let lifetime = writer.lit(0.6).uniform(writer.lit(1.3)).expr();
-    let init_lifetime = SetAttributeModifier::new(Attribute::LIFETIME, lifetime);
-
-    // Add drag to make particles slow down a bit after the initial acceleration
-    let drag = writer.lit(2.).expr();
-    let update_drag = LinearDragModifier::new(drag);
-
-    let mut module = writer.finish();
-
-    let tangent_accel = TangentAccelModifier::constant(&mut module, Vec3::ZERO, Vec3::Z, 30.);
-
-    let effect1 = effects.add(
-        EffectAsset::new(16384, SpawnerSettings::rate(5000.0.into()), module)
-            .with_name("portal")
-            .init(init_pos)
-            .init(init_age)
-            .init(init_lifetime)
-            .update(update_drag)
-            .update(tangent_accel)
-            .render(ColorOverLifetimeModifier::new(color_gradient1))
-            .render(SizeOverLifetimeModifier {
-                gradient: size_gradient1,
-                screen_space_size: false,
-            })
-            .render(OrientModifier::new(OrientMode::AlongVelocity)),
+    let mut size_tapering = Gradient::new();
+    size_tapering.add_key(0.0, Vec2::splat(0.4));
+    size_tapering.add_key(0.5, Vec2::slat(0.15));
+    size_tapering.add_key(1.0, Vec2::splat(0.0));
+    let mut module = Module::default();
+    let bottom_thruster = effects.add(
+        EffectAsset::new(32768, SpawnerSettings::rate(1000.0.into()), module)
+            
     );
     commands.spawn((
-        Name::new("portal"),
-        ParticleEffect::new(effect1),
+        Name::new("Thrusters"),
+        ParticleEffect::new(bottom_thruster),
         Transform::from_xyz(0.0, 10.0, 0.0),
     ));
 }
